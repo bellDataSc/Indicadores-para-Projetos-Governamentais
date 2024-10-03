87b7aca0d6cc0442da96ba493f6e1496d60cd75c
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2024-10-03 14:41:25 -0300

    Metodologia.py
    Um guia para seguir o passo a passo na aplicação de um projeto de indicadores governamentais.

---
+++ Metodologia.py
@@ -0,0 +1,9 @@
+#Aqui vou deixar um index, para melhor acesso ao meu trabalho.
+- 1. Preparação do Ambiente
+- 2. Construção do Banco de Dados
+- 3. Inserção de Dados
+- 4. Tratamento de Dados
+- 5. Consolidação dos Dados
+- 6. Documentação e Versionamento
+- 7. Versionamento no GitHub
+- Exemplo de projeto
