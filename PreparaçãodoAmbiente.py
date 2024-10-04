5dec7b474e9b682cc31905b3b51cf1ac23fda922
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2024-10-04 16:23:13 -0300

    Preparação do Ambiente.py
    Configuração de ambiente

---
+++ PreparaçãodoAmbiente.py
@@ -0,0 +1,6 @@
+#1. Preparação do Ambiente
+#Antes de tudo, certifique-se de ter o ambiente configurado corretamente.
+
+#Instalação de Bibliotecas Necessárias:
+
+  pip install pandas numpy sqlalchemy psycopg2
