6ff58012c9aecd83201281a5109018a09de3b646
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2024-10-12 17:30:26 -0300

    Documentação e Versionamento.lua
    #6. Documentação e Versionamento
    #Para manter um bom portfólio no GitHub, siga estas práticas:
    
    #Estrutura do Repositório

---
+++ DocumentaçãoeVersionamento.lua
@@ -0,0 +1,15 @@
+#6. Documentação e Versionamento
+#Para manter um bom portfólio no GitHub, siga estas práticas:
+
+#Estrutura do Repositório:
+
+    /meu_projeto
+|-- data/
+|   |-- raw/
+|   |-- processed/
+|-- notebooks/
+|-- scripts/
+|-- README.md
+|-- requirements.txt
+
+  
