37fb33f92553a230929a22cdae939380a1f14089
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2024-10-26 15:38:16 -0300

    VersionamentonoGitHub.py
    #7. Versionamento no GitHub

---
+++ VersionamentonoGitHub.py
@@ -0,0 +1,15 @@
+#7. Versionamento no GitHub
+# repositório
+git init
+
+# Adicionando arquivos
+git add .
+
+# Commitando mudanças
+git commit -m "Indicadores-para-Projetos-Governamentais"
+
+# Configurando o repositório remoto
+git remote add origin https://github.com/bellDataSc/Indicadores-para-Projetos-Governamentais.git
+
+# Enviando para o GitHub
+git push -u origin master
